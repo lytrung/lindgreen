17612a03b58b9116eacb4db6006d4a4c0ec1d016
Trung Ly <lytrung@gmail.com>
2020-03-24 11:39:20 +1300

    first commit

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+
+$subject = 'Lindgreen Fencing Website Enquiry';
+
+// In case any of our lines are larger than 70 characters, we should use wordwrap()
+$message = $_POST['FullName'].' from '.$_POST['EmailAddress'].' sent you a message: ';
+$message .= $_POST['CAT_Custom_20005859'];
+
+$to = 'nzmikejp@gmail.com';
+
+$headers = "From: Lindgreen Fencing Website <no-reply@lindgreenfencing.co.nz>\r\n";
+$headers .= "Reply-To: no-reply@lindgreenfencing.co.nz\r\n";
+
+// Send
+mail($to, $subject, $message, $headers);
+
+header('Location: index.html')
+?>
